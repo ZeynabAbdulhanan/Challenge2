2113788961cf9e59c26b46c04de4d06310f83df8
ZeynabAbdulhanan <61313171+ZeynabAbdulhanan@users.noreply.github.com>
2020-03-09 14:00:20 +0100

    schrijf & update js code

--- java.js
+++ java.js
@@ -1,1 +1,26 @@
-
+function myClock() {
+    var currentDate = new Date();
+    
+    var h = currentDate.getHours(); //hours, defining the variable currentDate
+    var m = currentDate.getMinutes(); //minutes 
+    var s = currentDate.getSeconds(); //seconds
+    
+    if(h < 24){
+        h = h - 4; 
+    }
+    
+    if(h == 0){
+        h = 24; 
+    }   
+    
+    if(m < 59){
+        m = m - 37;
+    }
+    
+    if(m == 0){
+        m == 59;
+    }
+    
+    var clock = document.getElementById("clock").innerHTML =   h + ":" + m + ":" + s;//id van div
+    var t = setTimeout(myClock, 500);//refresch every half second 
+}
